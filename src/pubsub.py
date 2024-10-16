df2523f0ba810ab8df6ff530bcf85bc89df7df3d
yth1122 <yxk1212@naver.com>
2024-10-16 16:08:27 +0900

    2024-10-16

--- src/pubsub.py
+++ src/pubsub.py
@@ -1,1 +1,19 @@
 from google.cloud import pubsub_v1
+
+project_id = "centered-sight"
+
+topic_id = "yhs_data"
+
+
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+
+def callback(message):
+    print(f"Received message: {message.data.decode('utf-8')}")
+    print(f"Attributes: {message.attributes}")
+    # 메시지 처리가 완료되면 승인(ack)합니다
+    message.ack()
+
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
